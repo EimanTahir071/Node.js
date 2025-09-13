25cf2c2ae9e849bd1541f981fe72865a226ff7a0
Eiman Tahir <143683612+EimanTahir071@users.noreply.github.com>
2025-09-14 02:51:18 +0500

    Add files via upload

---
+++ 2.2+Native+Modules/2.2 Native Modules/index.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+fs.writeFile("message.txt", "Hello World!", (err) => {
+    if (err) throw err;
+    console.log("File created");
+});
+
+fs.readFile("message.txt", "utf8", (err, data) => {
+    if (err) throw err;
+    console.log(data);
+});
